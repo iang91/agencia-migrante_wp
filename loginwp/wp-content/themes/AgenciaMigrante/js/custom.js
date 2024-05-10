8e5599c9942ae139ae1c5d08edf03e4017fe0bef
Ivan <ivan.doroteo1@gmail.com>
2024-05-10 11:25:29 -0700

    subi cambios a archivos js y css

--- loginwp/wp-content/themes/AgenciaMigrante/js/custom.js
+++ loginwp/wp-content/themes/AgenciaMigrante/js/custom.js
@@ -4,4 +4,49 @@ jQuery(function ($) {
 
     /* OPEN SOCIAL IN NEW TAB */
         $(".et-social-icon a").attr('target', '_blank');
+
+
+        
+        firstWord('.firstWord .et_pb_slide_title')
+
+        function firstWord(el) {
+        
+        
+           let element0 = document.querySelectorAll(el)
+        
+           try{
+        
+             target = element0
+              target.forEach(e=>{
+        
+                let element = e;
+        
+                if (e.textContent.includes('//')) {
+        
+                separator = e.textContent.split('//')
+                e.innerHTML = `<span class="light-font">${separator[0]}</span> ${separator[1]}`
+                
+               } else if(e.textContent.includes('|')){
+                separator2 = e.textContent.split('|')
+                e.innerHTML = `<span class="light-font">${separator2[0]}</span> <i>|</i> ${separator2[1]}`
+    
+               }else if(e.textContent.includes('--')){
+                separator3 = e.textContent.split('--')
+                e.innerHTML = `${separator3[0]} <span class="light-font">${separator3[1]}</span>`
+    
+               }
+           
+        
+            })
+        
+          }catch(err){
+            err
+        }
+        
+         
+        
+        }
+    
+    
+
 });
